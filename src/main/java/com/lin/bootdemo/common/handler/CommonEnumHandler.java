556559c893d449d0df76e9a069fb4099253abf1b
lcg <chuangang.lin@inrevo.io>
2020-01-14 11:06:46 +0800

    删除无用代码

--- src/main/java/com/lin/bootdemo/common/handler/CommonEnumHandler.java
+++ src/main/java/com/lin/bootdemo/common/handler/CommonEnumHandler.java
@@ -14,11 +14,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- * @author gd
- * @Date 2019/11/27 20:58
- * @Description work region type handler
- */
+
 @MappedTypes({BaseEnum.class})
 public class CommonEnumHandler<E extends Enum<E> & BaseEnum> extends BaseTypeHandler<E> {
 
